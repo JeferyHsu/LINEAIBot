db231f4a8f15788b04b78a439fbb83fade8fbb63
JeferyHsu <bcps10350113@gmail.com>
2025-05-12 02:05:23 +0800

    v13

--- app.py
+++ app.py
@@ -128,18 +128,24 @@ def handle_text_message(event):
 
 # 提取地點函數
 def extract_location(text):
+    # 將"台"轉換為"臺"
+    text = text.replace("台", "臺")
+
     location_mapping = {
         "宜蘭縣", "花蓮縣", "臺東縣", "澎湖縣", 
         "金門縣", "連江縣", "臺北市", "新北市",
         "桃園市", "臺中市", "臺南市", "高雄市", 
         "基隆市", "新竹縣", "新竹市", "苗栗縣",
         "彰化縣", "南投縣", "雲林縣", "嘉義縣", 
-        "嘉義市", "屏東縣"
+        "嘉義市", "屏東縣",
     }
     
     for loc in location_mapping.keys():
         if loc in text:
             return location_mapping[loc]
+        # 如果用戶只輸入了縣市名稱的前兩個字
+        elif loc[:2] in text:
+            return loc
     return None
 
 
